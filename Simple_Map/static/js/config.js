3f370d1c97723058edda425f87d30d41bee74053
Super-Manda <105459739+Super-Manda@users.noreply.github.com>
2022-08-27 00:05:36 -0400

    13.3.4

---
+++ Simple_Map/static/js/config.js
@@ -0,0 +1,6 @@
+// Enter your API key
+gkey = "AIzaSyBglkGBVpw4aQQ-GL9pgrB9AOkOuxCdpmU"
+g_key = "AIzaSyBglkGBVpw4aQQ-GL9pgrB9AOkOuxCdpmU"
+weather_api_key="b910b5fee2a572d059ee6667adc4ac23"
+
+const API_KEY = "pk.eyJ1IjoiYW1hbmRhZGVjaWxsaXMiLCJhIjoiY2w3NWM3cXF2MDRsejNwcWxhaHM2bmJ3cyJ9.Sk19cl_pp5Hq8sUN_Arcxw"
